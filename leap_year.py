d0e4fbb90f8dd63f5b13b5256ed96d53ce825f64
jkimel <jkimel@mail.austral.edu.ar>
2025-04-13 19:29:58 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
-def leap_year():
-    print("TO DO")
+def leap_year(fecha):
+    if (fecha % 4 == 0 and fecha % 100 != 0) or (fecha % 400 == 0):
+        return f"El año {fecha} es bisiesto"
+    else:
+        return f"El año {fecha} no es bisiesto"
+fecha_usuario = int(input("Ingrese un año: "))
+print(leap_year(fecha_usuario))
