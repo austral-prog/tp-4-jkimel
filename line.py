7837fd5831cbc9698dc119c73081c8c4bd22e977
jkimel <jkimel@mail.austral.edu.ar>
2025-04-14 09:58:08 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,22 +1,21 @@
-import math
 def line():
-    A= float(input('Ingrese el coeficiente A: '))
-    B= float(input('Ingrese el coeficiente B: '))
-    X1= float(input('Ingrese el coeficiente X1: '))
-    X2= float(input('Ingrese el coeficiente X2: '))
+    A = float(input('Ingrese el coeficiente A: '))
+    B = float(input('Ingrese el coeficiente B: '))
+    X1 = float(input('Ingrese el coeficiente X1: '))
+    X2 = float(input('Ingrese el coeficiente X2: '))
+    Y1 = A * X1 + B
+    Y2 = A * X2 + B
     print(f'El coeficiente A de su ecuación de la recta es: {A}')
     print(f'El coeficiente B de su ecuación de la recta es: {B}')
     print(f'El coeficiente X1 de su ecuación de la recta es: {X1}')
     print(f'El coeficiente X2 de su ecuación de la recta es: {X2}')
     print('')
     print('Para la siguiente ecuación:')
-    print('\t Y = 2.3X + -4.0')
+    print(f'\tY = {A}X + {B}')
     print('')
-    print('Dados los siguientes puntos:')
-    print('\t P1 (50.0, 110.99999999999999)')
-    print('\t P2 (-32.9, -79.66999999999999)')
+    print(f'Dados los siguientes puntos:')
+    print(f'\tP1 ({X1}, {Y1})')
+    print(f'\tP2 ({X2}, {Y2})')
     print('')
-    P1= (50.0, 110.99999999999999)
-    P2= (-32.9, -79.66999999999999)
-    distancia = math.dist(P1, P2)
+    distancia = ((X2 - X1)**2 + (Y2 - Y1)**2) ** 0.5  # Raíz cuadrada sin usar math
     print(f"La distancia entre ellos es: {distancia}")
